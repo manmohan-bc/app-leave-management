c2aa0da9dc5e5527b3095c998bc0147b4c6b28e4
manamohan <v-mnayak@simsupplychain.com>
2021-05-03 19:54:53 +0530

    Add project files.

---
+++ leave-management/Mappings/Maps.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using leave_management.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Mappings
+{
+    public class Maps : Profile
+    {
+        public Maps()
+        {
+            CreateMap<LeaveType, Models.DetailsLeaveTypeViewModel>().ReverseMap();
+            CreateMap<LeaveType, Models.CreateLaveTypeViewModel>().ReverseMap();
+
+            CreateMap<LeaveHistory, Models.LeaveHistoryViewModel>().ReverseMap();
+            
+            CreateMap<LeaveAllocation, Models.LeaveAllocationViewModel>().ReverseMap();
+            
+            CreateMap<Employee, Models.EmployeeViewModel>().ReverseMap();
+        }
+    }
+}
---
+++ leave-management/Repository/LeaveHistoryRepository.cs
@@ -0,0 +1,56 @@
+﻿using leave_management.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Repository
+{
+    public class LeaveHistoryRepository : Contracts.ILeaveHistoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        #region Contructor
+        public LeaveHistoryRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        #endregion
+
+        public bool Create(LeaveHistory entity)
+        {
+            _db.LeaveHistories.Add(entity);
+            return true;
+        }
+
+        public bool Delete(LeaveHistory entity)
+        {
+            _db.LeaveHistories.Remove(entity);
+            return Save();
+        }
+
+        public ICollection<LeaveHistory> FindAll()
+        {
+            var LeaveHistorys = _db.LeaveHistories.ToList();
+            return LeaveHistorys;
+        }
+
+        public LeaveHistory FindById(int id)
+        {
+            var LeaveHistory = _db.LeaveHistories.Find(id);
+            return LeaveHistory;
+        }
+
+        public bool Save()
+        {
+            var changes = _db.SaveChanges();
+            return changes > 0;
+        }
+
+        public bool Update(LeaveHistory entity)
+        {
+            _db.LeaveHistories.Update(entity);
+            return Save();
+        }
+    }
+}
